ac9fc6d98696bc8895dde886bfe24bc0713d606d
Mahdiar Shoraka <mahdiarshoraka@gmail.com>
2024-06-14 13:21:47 -0400

    Weapon Overlap + Enemy Added
    Weapon Overlap functions completed.
    Enemy Class Added

--- Source/Slash/Public/Items/Weapon.h
+++ Source/Slash/Public/Items/Weapon.h
@@ -18,9 +18,17 @@ public:
 	void Equip(USceneComponent* InParent, FName InSocketName);
 	void AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
 
+	// Getters and Setters
+	FORCEINLINE UBoxComponent* GetWeaponBox() const { return WeaponBox; }
+
 protected:
+	virtual void BeginPlay() override;
+
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UFUNCTION()
+	virtual void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
@@ -28,4 +36,10 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
 	UBoxComponent* WeaponBox;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceStart;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceEnd;
 };
